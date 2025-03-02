882a25497b065e2d762ad735474354bbf8f32027
Guido Günther <agx@sigxcpu.org>
2025-03-02 13:19:04 +0100

    main: Init libfeedback
    Needed to emit haptic
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/186>

--- src/main.c
+++ src/main.c
@@ -9,18 +9,26 @@
 #include "mobile-settings-config.h"
 #include "mobile-settings-application.h"
 
+#include <libfeedback.h>
+
 int
 main (int argc, char *argv[])
 {
   g_autoptr (MobileSettingsApplication) app = NULL;
+  g_autoptr (GError) err = NULL;
   int ret;
 
   bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   textdomain (GETTEXT_PACKAGE);
 
+  if (!lfb_init (MOBILE_SETTINGS_APP_ID, &err))
+    g_warning ("Failed to init libfeedback: %s", err->message);
+
   app = mobile_settings_application_new (MOBILE_SETTINGS_APP_ID);
   ret = g_application_run (G_APPLICATION (app), argc, argv);
+
+  lfb_uninit ();
 
   return ret;
 }
