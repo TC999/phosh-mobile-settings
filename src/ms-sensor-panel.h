ddbf53abe92ca7cb3c23b2289cfc1c7abfb9ff21
Rudra Pratap Singh <rudrasingh900@gmail.com>
2025-06-26 01:50:35 +0530

    sensor-panel: Derive from MsPanel class
    Signed-off-by: Rudra Pratap Singh <rudrasingh900@gmail.com>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/216>

--- src/ms-sensor-panel.h
+++ src/ms-sensor-panel.h
@@ -6,13 +6,13 @@
 
 #pragma once
 
-#include <adwaita.h>
+#include "ms-panel.h"
 
 G_BEGIN_DECLS
 
 #define MS_TYPE_SENSOR_PANEL (ms_sensor_panel_get_type ())
 
-G_DECLARE_FINAL_TYPE (MsSensorPanel, ms_sensor_panel, MS, SENSOR_PANEL, AdwBin)
+G_DECLARE_FINAL_TYPE (MsSensorPanel, ms_sensor_panel, MS, SENSOR_PANEL, MsPanel)
 
 MsSensorPanel *ms_sensor_panel_new (void);
 
