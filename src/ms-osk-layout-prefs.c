TC999/phosh-mobile-settings

commit 4ac327db8f33dd724755b5a105ab8853032ff5ff

osk-panel: Allow to configure OSK layouts
This allows to add / remove and sort keyboard layouts.

The list of available layouts is provided by the OSK. For XKB layouts we
provide a translated language from the xkb database.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/154>

src/ms-osk-layout-prefs.c
@@ -1,0 +1,533 @@
+/*
+ * Copyright (C) 2024 The Phosh Developers
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#define G_LOG_DOMAIN "ms-osk-layout-prefs"
+
+#include "mobile-settings-config.h"
+
+#include "ms-osk-add-layout-dialog.h"
+#include "ms-osk-layout-prefs.h"
+#include "ms-osk-layout-row.h"
+#include "ms-osk-layout.h"
+
+#include <json-glib/json-glib.h>
+
+#define GNOME_DESKTOP_USE_UNSTABLE_API
+#include <libgnome-desktop/gnome-xkb-info.h>
+
+#include <gtk/gtk.h>
+
+/**
+ * MsOskLayoutPrefs:
+ *
+ * A preferences prefs managing OSK layouts
+ */
+
+#define INPUT_SOURCES_SETTINGS "org.gnome.desktop.input-sources"
+#define SOURCES_KEY            "sources"
+
+struct _MsOskLayoutPrefs {
+  AdwPreferencesGroup   parent;
+
+  GtkWidget            *layouts_preferences_group;
+  GtkWidget            *layouts_list_box;
+
+  /* Currently configured layouts in input-sources */
+  GListStore           *source_layouts;
+  GSettings            *input_source_settings;
+  GVariant             *sources;
+  GnomeXkbInfo         *xkbinfo;
+
+  /* All layouts the OSK supports */
+  GListStore           *available_layouts;
+  GHashTable           *available_layouts_by_id;
+
+  /* Enabled layouts in the input sources that the OSK supports */
+  GtkFilterListModel   *usable_layouts;
+  GtkCustomFilter      *usable_filter;
+
+  GCancellable         *cancel;
+};
+G_DEFINE_TYPE (MsOskLayoutPrefs, ms_osk_layout_prefs, ADW_TYPE_PREFERENCES_GROUP)
+
+
+static void
+update_enabled_move_actions (MsOskLayoutPrefs *self)
+{
+  GtkWidget *child;
+
+  for (child = gtk_widget_get_first_child (GTK_WIDGET (self->layouts_list_box));
+       child != NULL;
+       child = gtk_widget_get_next_sibling (child)) {
+    gint row_idx;
+    GtkWidget *sibling;
+
+    if (!MS_IS_OSK_LAYOUT_ROW (child))
+      continue;
+
+    row_idx = gtk_list_box_row_get_index (GTK_LIST_BOX_ROW (child));
+    gtk_widget_action_set_enabled (GTK_WIDGET (child), "row.move-up", row_idx != 0);
+
+
+    sibling = gtk_widget_get_next_sibling (GTK_WIDGET (child));
+    gtk_widget_action_set_enabled (GTK_WIDGET (child), "row.move-down",
+                                   MS_IS_OSK_LAYOUT_ROW (sibling));
+  }
+}
+
+
+static void
+update_remove_actions (MsOskLayoutPrefs *self)
+{
+  GtkWidget *child;
+  gboolean enable;
+
+  enable = g_list_model_get_n_items (G_LIST_MODEL (self->usable_layouts)) > 1;
+
+  for (child = gtk_widget_get_first_child (GTK_WIDGET (self->layouts_list_box));
+       child != NULL;
+       child = gtk_widget_get_next_sibling (child)) {
+
+    if (!MS_IS_OSK_LAYOUT_ROW (child))
+      continue;
+
+    gtk_widget_action_set_enabled (GTK_WIDGET (child), "row.remove", enable);
+  }
+}
+
+
+static char *
+get_osk_layout_name (MsOskLayoutPrefs *self, const char *type, const char *layout_id)
+{
+  MsOskLayout *layout = NULL;
+  g_autofree char *key = NULL;
+
+  key = g_strdup_printf ("%s:%s", type, layout_id);
+  layout = g_hash_table_lookup (self->available_layouts_by_id, key);
+
+  if (!layout)
+    return NULL;
+
+  return g_strdup (ms_osk_layout_get_name (layout));
+}
+
+
+static void
+on_input_sources_changed (MsOskLayoutPrefs *self, const char *unused, GSettings *settings)
+{
+  const char *type, *layout_id;
+  GVariantIter iter;
+
+  g_debug ("Setting changed, reloading input settings");
+
+  g_clear_pointer (&self->sources, g_variant_unref);
+  self->sources = g_settings_get_value (settings, SOURCES_KEY);
+
+  g_list_store_remove_all (self->source_layouts);
+
+  g_variant_iter_init (&iter, self->sources);
+  while (g_variant_iter_next (&iter, "(&s&s)", &type, &layout_id)) {
+    g_autoptr (MsOskLayout) layout = NULL;
+    g_autofree char *name = NULL;
+
+    name = get_osk_layout_name (self, type, layout_id);
+    if (!name)
+      g_debug ("Failed to get name for %s %s", type, layout_id);
+
+    /* Even without a name we don't drop a layout as the user
+     * might want to use those in docked mode */
+
+    layout = ms_osk_layout_new (name, type, layout_id);
+    g_list_store_append (self->source_layouts, layout);
+  }
+
+  gtk_filter_changed (GTK_FILTER (self->usable_filter), GTK_FILTER_CHANGE_DIFFERENT);
+  update_enabled_move_actions (self);
+  update_remove_actions (self);
+}
+
+
+static void
+update_input_sources (MsOskLayoutPrefs *self)
+{
+  guint n_layouts;
+  GVariantBuilder builder;
+
+  g_variant_builder_init (&builder, G_VARIANT_TYPE ("a(ss)"));
+
+  n_layouts = g_list_model_get_n_items (G_LIST_MODEL (self->source_layouts));
+  for (guint i = 0; i < n_layouts; i++) {
+    MsOskLayout *layout = g_list_model_get_item (G_LIST_MODEL (self->source_layouts), i);
+
+    g_variant_builder_add (&builder, "(ss)",
+                           ms_osk_layout_get_type_ (layout),
+                           ms_osk_layout_get_id (layout));
+  }
+
+  g_settings_set_value (self->input_source_settings, SOURCES_KEY, g_variant_builder_end (&builder));
+}
+
+
+static void
+on_layout_selected (MsOskLayoutPrefs *self, MsOskLayout *layout)
+{
+  g_assert (MS_IS_OSK_LAYOUT_PREFS (self));
+  g_assert (MS_IS_OSK_LAYOUT (layout));
+
+  g_signal_handlers_block_by_func (self->input_source_settings,
+                                   on_input_sources_changed,
+                                   self);
+
+  g_list_store_append (self->source_layouts, layout);
+  update_input_sources (self);
+
+  g_signal_handlers_unblock_by_func (self->input_source_settings,
+                                     on_input_sources_changed,
+                                     self);
+
+  on_input_sources_changed (self, NULL, self->input_source_settings);
+}
+
+
+static gboolean
+is_source_layout (MsOskLayoutPrefs *self, MsOskLayout *layout)
+{
+  const char *type, *layout_id;
+  GVariantIter iter;
+
+  g_variant_iter_init (&iter, self->sources);
+  while (g_variant_iter_next (&iter, "(&s&s)", &type, &layout_id)) {
+    if (g_str_equal (type, ms_osk_layout_get_type_ (layout)) &&
+        g_str_equal (layout_id, ms_osk_layout_get_id (layout))) {
+      return TRUE;
+    }
+  }
+
+  return FALSE;
+}
+
+
+static void
+on_add_layout_activated (GtkWidget *widget, const char *action_name, GVariant *parameter)
+{
+  MsOskLayoutPrefs *self = MS_OSK_LAYOUT_PREFS (widget);
+  GtkWidget *dialog;
+  g_autoptr (GListStore) layouts = g_list_store_new (MS_TYPE_OSK_LAYOUT);
+  guint n_items;
+
+  n_items = g_list_model_get_n_items (G_LIST_MODEL (self->available_layouts));
+  for (int i = 0; i < n_items; i++) {
+    g_autoptr (MsOskLayout) layout = NULL;
+
+    layout = g_list_model_get_item (G_LIST_MODEL (self->available_layouts), i);
+    if (is_source_layout (self, layout))
+      continue;
+
+    g_list_store_append (layouts, layout);
+  }
+
+  dialog = ms_osk_add_layout_dialog_new (G_LIST_MODEL (layouts));
+  g_signal_connect_object (dialog,
+                           "layout-selected",
+                           G_CALLBACK (on_layout_selected),
+                           self,
+                           G_CONNECT_SWAPPED);
+
+  adw_dialog_present (ADW_DIALOG (dialog), widget);
+}
+
+
+static gboolean
+filter_usable (MsOskLayout *layout, MsOskLayoutPrefs *self)
+{
+  g_autofree char *key = NULL;
+
+  /* Assume all are usable if we couldn't get layout info */
+  if (!self->available_layouts)
+    return TRUE;
+
+  key = g_strdup_printf ("%s:%s",
+                         ms_osk_layout_get_type_ (layout),
+                         ms_osk_layout_get_id (layout));
+  return g_hash_table_contains (self->available_layouts_by_id, key);
+}
+
+
+
+static void
+on_row_moved (MsOskLayoutPrefs *self, MsOskLayoutRow *dest_row, MsOskLayoutRow *row)
+{
+  guint idx, dest_idx;
+  MsOskLayout *layout, *dest_layout;
+  gboolean success;
+
+  layout = ms_osk_layout_row_get_layout (row);
+  g_assert (layout);
+  success = g_list_store_find (self->source_layouts, layout, &idx);
+  g_assert (success);
+
+  dest_layout = ms_osk_layout_row_get_layout (dest_row);
+  g_assert (dest_layout);
+  success = g_list_store_find (self->source_layouts, dest_layout, &dest_idx);
+  g_assert (success);
+
+  g_signal_handlers_block_by_func (self->input_source_settings,
+                                   on_input_sources_changed,
+                                   self);
+  /* The row holds a ref on layout so it won't get destroyed on
+   * removal */
+  g_list_store_remove (self->source_layouts, idx);
+  g_list_store_insert (self->source_layouts, dest_idx, layout);
+
+  update_input_sources (self);
+
+  g_signal_handlers_unblock_by_func (self->input_source_settings,
+                                     on_input_sources_changed,
+                                     self);
+
+  on_input_sources_changed (self, NULL, self->input_source_settings);
+}
+
+
+static void
+on_row_removed (MsOskLayoutPrefs *self, MsOskLayoutRow *dest_row, MsOskLayoutRow *row)
+{
+  guint idx;
+  MsOskLayout *layout;
+  gboolean success;
+
+  layout = ms_osk_layout_row_get_layout (row);
+  g_assert (layout);
+  success = g_list_store_find (self->source_layouts, layout, &idx);
+  g_assert (success);
+
+  g_signal_handlers_block_by_func (self->input_source_settings,
+                                   on_input_sources_changed,
+                                   self);
+  /* The row holds a ref on layout so it won't get destroyed on
+   * removal */
+  g_list_store_remove (self->source_layouts, idx);
+
+  update_input_sources (self);
+
+  g_signal_handlers_unblock_by_func (self->input_source_settings,
+                                     on_input_sources_changed,
+                                     self);
+
+  on_input_sources_changed (self, NULL, self->input_source_settings);
+}
+
+
+static GtkWidget *
+create_layout_row (gpointer item, gpointer user_data)
+{
+  MsOskLayoutPrefs *self = MS_OSK_LAYOUT_PREFS (user_data);
+  MsOskLayout *layout = MS_OSK_LAYOUT (item);
+  GtkWidget *row;
+
+  row = ms_osk_layout_row_new (layout);
+  g_signal_connect_object (row, "move-row", G_CALLBACK (on_row_moved), self, G_CONNECT_SWAPPED);
+  g_signal_connect_object (row, "remove-row",
+                           G_CALLBACK (on_row_removed),
+                           self,
+                           G_CONNECT_SWAPPED);
+
+  return row;
+}
+
+
+static void
+on_load_osk_layouts_from_stream_ready (JsonParser *parser, GAsyncResult *res, gpointer user_data)
+{
+  gboolean success;
+  g_autoptr (GError) err = NULL;
+  MsOskLayoutPrefs *self = NULL;
+  JsonNode *node;
+  JsonObject *obj;
+  JsonArray *layouts;
+
+  success = json_parser_load_from_stream_finish  (parser, res, &err);
+  if (!success) {
+    g_warning ("Failed to load layouts: %s", err->message);
+    return;
+  }
+
+  self = MS_OSK_LAYOUT_PREFS (user_data);
+  g_assert (MS_IS_OSK_LAYOUT_PREFS (self));
+
+  node = json_parser_get_root (parser);
+
+  obj = json_node_get_object (node);
+  if (!obj) {
+    g_warning ("Failed to get root object");
+    return;
+  }
+
+  layouts = json_object_get_array_member (obj, "layouts");
+  if (!obj) {
+    g_warning ("Failed to get array of layouts");
+    return;
+  }
+
+  for (int i = 0; i < json_array_get_length (layouts); i++) {
+    g_autoptr (MsOskLayout) layout = NULL;
+    JsonObject *layout_obj;
+    const char *name, *type, *layout_id, *display_name;
+    g_autofree char *key = NULL, *layout_name = NULL;
+
+    layout_obj = json_array_get_object_element (layouts, i);
+    g_assert (layout_obj);
+
+    name = json_object_get_string_member (layout_obj, "name");
+    type = json_object_get_string_member (layout_obj, "type");
+    layout_id = json_object_get_string_member (layout_obj, "layout-id");
+
+    if (!name || !type || !layout_id) {
+      g_warning ("Skipping layout %d", i);
+      continue;
+    }
+
+    /* Get a nicer (translated) display name for xkb layouts */
+    if (g_str_equal (type, "xkb")) {
+      if (gnome_xkb_info_get_layout_info (self->xkbinfo, layout_id, &display_name,
+                                          NULL, NULL, NULL)) {
+        layout_name = g_strdup (display_name);
+      }
+    }
+
+    if (!layout_name)
+      layout_name = g_strdup (name);
+
+    g_debug ("Adding layout %s", layout_name);
+    layout = ms_osk_layout_new (layout_name, type, layout_id);
+    g_list_store_append (self->available_layouts, layout);
+
+    key = g_strdup_printf ("%s:%s", type, layout_id);
+    g_hash_table_insert (self->available_layouts_by_id,
+                         g_steal_pointer (&key),
+                         g_steal_pointer (&layout));
+  }
+
+  /* Reload the layouts as we got more layout information */
+  on_input_sources_changed (self, SOURCES_KEY, self->input_source_settings);
+}
+
+
+static void
+ms_osk_layout_prefs_dispose (GObject *object)
+{
+  MsOskLayoutPrefs *self = MS_OSK_LAYOUT_PREFS (object);
+
+  g_cancellable_cancel (self->cancel);
+  g_clear_object (&self->cancel);
+
+  g_clear_object (&self->usable_filter);
+  g_clear_object (&self->usable_layouts);
+
+  g_clear_pointer (&self->available_layouts_by_id, g_hash_table_destroy);
+  g_clear_object (&self->available_layouts);
+
+  g_clear_object (&self->xkbinfo);
+  g_clear_pointer (&self->sources, g_variant_unref);
+  g_clear_object (&self->input_source_settings);
+  g_clear_object (&self->source_layouts);
+
+  G_OBJECT_CLASS (ms_osk_layout_prefs_parent_class)->dispose (object);
+}
+
+
+static void
+ms_osk_layout_prefs_class_init (MsOskLayoutPrefsClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->dispose = ms_osk_layout_prefs_dispose;
+
+
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/mobi/phosh/MobileSettings/ui/"
+                                               "ms-osk-layout-prefs.ui");
+
+  gtk_widget_class_install_action (widget_class, "osk-prefs.add-layout", NULL,
+                                   on_add_layout_activated);
+
+  gtk_widget_class_bind_template_child (widget_class, MsOskLayoutPrefs, layouts_list_box);
+}
+
+
+static void
+ms_osk_layout_prefs_init (MsOskLayoutPrefs *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  self->cancel = g_cancellable_new ();
+  self->xkbinfo = gnome_xkb_info_new ();
+
+  self->available_layouts = g_list_store_new (MS_TYPE_OSK_LAYOUT);
+  self->available_layouts_by_id = g_hash_table_new_full (g_str_hash,
+                                                         g_str_equal,
+                                                         g_free,
+                                                         g_object_unref);
+
+  self->source_layouts = g_list_store_new (MS_TYPE_OSK_LAYOUT);
+  self->usable_filter = gtk_custom_filter_new ((GtkCustomFilterFunc)filter_usable, self, NULL);
+  g_object_ref (self->source_layouts);
+  g_object_ref (self->usable_filter);
+  self->usable_layouts = gtk_filter_list_model_new (G_LIST_MODEL (self->source_layouts),
+                                                    GTK_FILTER (self->usable_filter));
+  gtk_list_box_bind_model (GTK_LIST_BOX (self->layouts_list_box),
+                           G_LIST_MODEL (self->usable_layouts),
+                           create_layout_row,
+                           self,
+                           NULL);
+
+  self->input_source_settings = g_settings_new (INPUT_SOURCES_SETTINGS);
+  g_signal_connect_swapped (self->input_source_settings, "changed::" SOURCES_KEY,
+                            G_CALLBACK (on_input_sources_changed),
+                            self);
+  on_input_sources_changed (self, SOURCES_KEY, self->input_source_settings);
+}
+
+
+MsOskLayoutPrefs *
+ms_osk_layout_prefs_new (void)
+{
+  return g_object_new (MS_TYPE_OSK_LAYOUT_PREFS, NULL);
+}
+
+/**
+ * ms_osk_layout_prefs_load_osk_layouts:
+ * @self: The OSK layout prefs
+ *
+ * Load the layouts the currently running OSK can handle.
+ */
+void
+ms_osk_layout_prefs_load_osk_layouts (MsOskLayoutPrefs *self)
+{
+  const char *layouts_path;
+  g_autoptr (GFile) layouts_file = NULL;
+  g_autoptr (JsonParser) parser = json_parser_new_immutable ();
+  g_autoptr (GFileInputStream) stream = NULL;
+  g_autoptr (GError) err = NULL;
+
+  layouts_path = getenv ("MS_OSK_LAYOUTS") ?: MOBILE_SETTINGS_OSK_LAYOUTS;
+  layouts_file = g_file_new_for_path (layouts_path);
+
+  stream = g_file_read (layouts_file, self->cancel, &err);
+  if (!stream) {
+    g_warning ("Can't load keyboard layouts: %s", err->message);
+    return;
+  }
+
+  json_parser_load_from_stream_async (parser,
+                                      G_INPUT_STREAM (stream),
+                                      self->cancel,
+                                      (GAsyncReadyCallback)on_load_osk_layouts_from_stream_ready,
+                                      self);
+}

debian/control
@@ -9,7 +9,9 @@ Build-Depends:
  libadwaita-1-dev (>= 1.5),
  libgsound-dev,
  libgmobile-dev (>= 0.2.0),
+ libgnome-desktop-4-dev,
  libgtk-4-dev,
+ libjson-glib-dev,
  libsensors-dev,
  meson,
  phosh-dev (>= 0.40.0),

meson.build
@@ -41,9 +41,11 @@ gmobile_dep = dependency('gmobile',
       'gtk_doc=false',
       'tests=false'
     ])
+gnome_desktop_dep = dependency('gnome-desktop-4', version: '>= 44')
 gsound_dep = dependency('gsound')
 gtk_dep = dependency('gtk4', version: gtk_ver_cmp)
 gtk_wayland_dep = dependency('gtk4-wayland', version: gtk_ver_cmp)
+json_glib_dep = dependency('json-glib-1.0', version: '>= 1.6.2')
 phosh_plugins_dep = dependency('phosh-plugins', version: '>= 0.23.0')
 phosh_settings_dep = dependency('phosh-settings', version: '>= 0.40.0')
 wayland_client_dep = dependency('wayland-client', version: '>=1.14')
@@ -143,6 +145,8 @@ config_h.set_quoted('MOBILE_SETTINGS_PHOSH_PLUGINS_DIR',
 		    phosh_plugins_dep.get_variable(pkgconfig: 'lockscreen_plugins_dir'))
 config_h.set_quoted('MOBILE_SETTINGS_PHOSH_PREFS_DIR',
 		    phosh_plugins_dep.get_variable(pkgconfig: 'lockscreen_prefs_dir'))
+config_h.set_quoted('MOBILE_SETTINGS_OSK_LAYOUTS', datadir / 'phosh-osk-stub' / 'layouts.json')
+
 configure_file(
   output: 'mobile-settings-config.h',
   configuration: config_h,

po/POTFILES.in
@@ -16,6 +16,9 @@ src/ui/ms-convergence-panel.ui
 src/ui/ms-features-panel.ui
 src/ui/ms-feedback-panel.ui
 src/ui/ms-lockscreen-panel.ui
+src/ui/ms-osk-add-layout-dialog.ui
+src/ui/ms-osk-layout-prefs.ui
+src/ui/ms-osk-layout-row.ui
 src/ui/ms-osk-panel.ui
 src/ui/ms-plugin-row.ui
 src/ui/ms-scale-to-fit-row.ui

src/meson.build
@@ -34,6 +34,14 @@ mobile_settings_sources = [
   'ms-head-tracker.h',
   'ms-lockscreen-panel.c',
   'ms-lockscreen-panel.h',
+  'ms-osk-add-layout-dialog.c',
+  'ms-osk-add-layout-dialog.h',
+  'ms-osk-layout.c',
+  'ms-osk-layout.h',
+  'ms-osk-layout-prefs.c',
+  'ms-osk-layout-prefs.h',
+  'ms-osk-layout-row.c',
+  'ms-osk-layout-row.h',
   'ms-osk-panel.c',
   'ms-osk-panel.h',
   'ms-panel-switcher.c',
@@ -71,9 +79,11 @@ mobile_settings_deps = [
   glib_dep,
   gmobile_dep,
   gmodule_dep,
+  gnome_desktop_dep,
   gsound_dep,
   gtk_dep,
   gtk_wayland_dep,
+  json_glib_dep,
   phosh_plugins_dep,
   phosh_settings_dep,
   wayland_client_dep,

src/mobile-settings.gresource.xml
@@ -9,6 +9,9 @@
     <file>ui/ms-feedback-panel.ui</file>
     <file>ui/ms-feedback-row.ui</file>
     <file>ui/ms-lockscreen-panel.ui</file>
+    <file>ui/ms-osk-add-layout-dialog.ui</file>
+    <file>ui/ms-osk-layout-prefs.ui</file>
+    <file>ui/ms-osk-layout-row.ui</file>
     <file>ui/ms-osk-panel.ui</file>
     <file>ui/ms-panel-switcher.ui</file>
     <file>ui/ms-plugin-row.ui</file>

src/ms-osk-add-layout-dialog.c
@@ -1,0 +1,208 @@
+/*
+ * Copyright (C) 2024 The Phosh Developers
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#define G_LOG_DOMAIN "ms-osk-add-layout-dialog"
+
+#include "mobile-settings-config.h"
+
+#include "ms-osk-add-layout-dialog.h"
+#include "ms-osk-layout.h"
+
+/**
+ * MsOskAddLayoutDialog:
+ *
+ * Dialog to add an OSK layout
+ */
+
+enum {
+  PROP_0,
+  PROP_LAYOUTS,
+  PROP_LAST_PROP
+};
+static GParamSpec *props[PROP_LAST_PROP];
+
+
+enum {
+  LAYOUT_SELECTED,
+  N_SIGNALS
+};
+static guint signals[N_SIGNALS];
+
+
+struct _MsOskAddLayoutDialog {
+  AdwDialog           parent;
+
+  GtkWidget          *add_button;
+  GtkListBox         *layouts_listbox;
+
+  GtkFilterListModel *model;
+};
+G_DEFINE_TYPE (MsOskAddLayoutDialog, ms_osk_add_layout_dialog, ADW_TYPE_DIALOG)
+
+
+static void
+on_search_changed (MsOskAddLayoutDialog *self, GtkSearchEntry *entry)
+{
+  const char *text;
+  GtkStringFilter *filter;
+
+  text = gtk_editable_get_text (GTK_EDITABLE (entry));
+  filter = GTK_STRING_FILTER (gtk_filter_list_model_get_filter (self->model));
+  gtk_string_filter_set_search (filter, text);
+}
+
+
+static void
+on_row_selected (MsOskAddLayoutDialog *self)
+{
+  gtk_widget_set_sensitive (self->add_button, TRUE);
+}
+
+
+static void
+on_add_clicked (MsOskAddLayoutDialog *self)
+{
+  GtkListBoxRow *row = gtk_list_box_get_selected_row (self->layouts_listbox);
+  MsOskLayout *layout;
+  int index;
+
+  index = gtk_list_box_row_get_index (row);
+  layout = g_list_model_get_item (G_LIST_MODEL (self->model), index);
+
+  g_signal_emit (self, signals[LAYOUT_SELECTED], 0, layout);
+
+  adw_dialog_close (ADW_DIALOG (self));
+}
+
+
+static GtkWidget *
+create_layout_row (gpointer item, gpointer user_data)
+{
+  MsOskLayout *layout = MS_OSK_LAYOUT (item);
+  GtkWidget *row;
+
+  row = adw_action_row_new ();
+  adw_preferences_row_set_title (ADW_PREFERENCES_ROW (row), ms_osk_layout_get_name (layout));
+
+  return row;
+}
+
+
+static void
+set_layouts (MsOskAddLayoutDialog *self, GListModel *layouts)
+{
+  GtkSorter *sorter;
+  GtkStringFilter *filter;
+  GtkSortListModel *sorted;
+  GtkExpression *expression;
+
+  expression = gtk_property_expression_new (MS_TYPE_OSK_LAYOUT, NULL, "name");
+  sorter = GTK_SORTER (gtk_string_sorter_new (expression));
+
+  expression = gtk_property_expression_new (MS_TYPE_OSK_LAYOUT, NULL, "name");
+  filter = gtk_string_filter_new (expression);
+  gtk_string_filter_set_ignore_case (filter, TRUE);
+  gtk_string_filter_set_match_mode (filter, GTK_STRING_FILTER_MATCH_MODE_PREFIX);
+
+  /* gtk_sort_list_model takes ownership */
+  g_object_ref (layouts);
+  sorted = gtk_sort_list_model_new (layouts, sorter);
+  self->model = gtk_filter_list_model_new (G_LIST_MODEL (sorted),
+                                           GTK_FILTER (filter));
+
+  gtk_list_box_bind_model (GTK_LIST_BOX (self->layouts_listbox),
+                           G_LIST_MODEL (self->model),
+                           create_layout_row,
+                           self,
+                           NULL);
+}
+
+
+static void
+ms_osk_add_layout_dialog_set_property (GObject      *object,
+                                       guint         property_id,
+                                       const GValue *value,
+                                       GParamSpec   *pspec)
+{
+  MsOskAddLayoutDialog *self = MS_OSK_ADD_LAYOUT_DIALOG (object);
+
+  switch (property_id) {
+  case PROP_LAYOUTS:
+    set_layouts (self, g_value_get_object (value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static void
+ms_osk_add_layout_dialog_dispose (GObject *object)
+{
+  MsOskAddLayoutDialog *self = MS_OSK_ADD_LAYOUT_DIALOG (object);
+
+  g_clear_object (&self->model);
+
+  G_OBJECT_CLASS (ms_osk_add_layout_dialog_parent_class)->dispose (object);
+}
+
+
+static void
+ms_osk_add_layout_dialog_class_init (MsOskAddLayoutDialogClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->set_property = ms_osk_add_layout_dialog_set_property;
+  object_class->dispose = ms_osk_add_layout_dialog_dispose;
+
+  props[PROP_LAYOUTS] =
+    g_param_spec_object ("layouts", "", "",
+                         G_TYPE_LIST_MODEL,
+                         G_PARAM_WRITABLE | G_PARAM_CONSTRUCT_ONLY);
+
+  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
+
+  signals[LAYOUT_SELECTED] =
+    g_signal_new ("layout-selected",
+                  G_TYPE_FROM_CLASS (object_class),
+                  G_SIGNAL_RUN_LAST,
+                  0,
+                  NULL, NULL,
+                  NULL,
+                  G_TYPE_NONE,
+                  1, MS_TYPE_OSK_LAYOUT);
+
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/mobi/phosh/MobileSettings/"
+                                               "ui/ms-osk-add-layout-dialog.ui");
+
+  gtk_widget_class_bind_template_child (widget_class, MsOskAddLayoutDialog, layouts_listbox);
+  gtk_widget_class_bind_template_child (widget_class, MsOskAddLayoutDialog, add_button);
+
+  gtk_widget_class_bind_template_callback (widget_class, on_add_clicked);
+  gtk_widget_class_bind_template_callback (widget_class, on_row_selected);
+  gtk_widget_class_bind_template_callback (widget_class, on_search_changed);
+}
+
+
+static void
+ms_osk_add_layout_dialog_init (MsOskAddLayoutDialog *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
+
+
+GtkWidget *
+ms_osk_add_layout_dialog_new (GListModel *layouts)
+{
+  return g_object_new (MS_TYPE_OSK_ADD_LAYOUT_DIALOG,
+                       "layouts", layouts,
+                       NULL);
+}

src/ms-osk-add-layout-dialog.h
@@ -1,0 +1,20 @@
+/*
+ * Copyright (C) 2024 The Phosh Developers
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <adwaita.h>
+
+G_BEGIN_DECLS
+
+#define MS_TYPE_OSK_ADD_LAYOUT_DIALOG (ms_osk_add_layout_dialog_get_type ())
+
+G_DECLARE_FINAL_TYPE (MsOskAddLayoutDialog, ms_osk_add_layout_dialog, MS, OSK_ADD_LAYOUT_DIALOG,
+                      AdwDialog)
+
+GtkWidget *ms_osk_add_layout_dialog_new (GListModel *layouts);
+
+G_END_DECLS

commit d0f466587acb2ffb9642ac3b77f6ec858c78a516

osk-layout-prefs: Fix row removal
The signature was wrong causing a crash when removing layouts.

Fixes: 4ac327d ("osk-panel: Allow to configure OSK layouts")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/223>

src/ms-osk-layout-prefs.c
@@ -295,7 +295,7 @@ on_row_moved (MsOskLayoutPrefs *self, MsOskLayoutRow *dest_row, MsOskLayoutRow *
 
 
 static void
-on_row_removed (MsOskLayoutPrefs *self, MsOskLayoutRow *dest_row, MsOskLayoutRow *row)
+on_row_removed (MsOskLayoutPrefs *self, MsOskLayoutRow *row)
 {
   guint idx;
   MsOskLayout *layout;